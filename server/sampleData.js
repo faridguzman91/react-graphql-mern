d66403cc132d93d4c3bc30776fe768fe38def157
faridguzman91 <farid.guzman91@hotmail.com>
2022-06-07 19:46:45 +0200

    random data

---
+++ server/sampleData.js
@@ -0,0 +1,87 @@
+// projects
+
+const projects = [
+    {
+        id: 1,
+        clientId: '1',
+        name: 'eCommerce Website',
+        description: 'This is a sample project',
+        startDate: '2019-01-01',
+        status: 'In Progress',
+    },
+    {
+        id: 2,
+        clientId: '2',
+        name: 'Dating App',
+        description: 'This is a sample project',
+        startDate: '2019-01-01',
+        status: 'In Progress',
+    },
+    {
+        id: 3,
+        clientId: '3',
+        name: 'Social Network',
+        description: 'This is a sample project',
+        startDate: '2019-01-01',
+        status: 'In Progress',
+    },
+    {
+        id: 4,
+        clientId: '4',
+        name: 'SaaS App',
+        description: 'This is a sample project',
+        startDate: '2019-01-01',
+        status: 'In Progress',
+    },
+    {
+        id: 5,
+        clientId: '5',
+        name: 'Food Delivery App',
+        description: 'This is a sample project',
+        startDate: '2019-01-01',
+        status: 'In Progress',
+    },
+
+
+]
+
+const clients = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@doe.com',
+        phone: '+1 (123) 456-7890',
+        address: '123 Main Street, Anytown, CA 12345',
+
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        email: 'jane@doe.com',
+        phone: '+1 (123) 456-7890',
+        address: '123 Main Street, Anytown, CA 12345',
+    },
+    {
+        id: 3,
+        name: 'Sam Smith',
+        email: 'sam@smith.com',
+        phone: '+1 (123) 456-7890',
+        address: '123 Main Street, Anytown, CA 12345',
+    },
+    {
+        id: 4,
+        name: 'Tom Tom',
+        email: 'tom@tom.com',
+        phone: '+1 (123) 456-7890',
+        address: '123 Main Street, Anytown, CA 12345',
+    },
+    {
+        id: 5,
+        name: 'John Doe',
+        email: '2john2doe@doe.com',
+        phone: '+1 (123) 456-7890',
+        address: '123 Main Street, Anytown, CA 12345',
+    },
+]
+
+module.exports = { projects, clients };
